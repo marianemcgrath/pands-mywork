ab848914159e236f486c640443bca387c2ac4e43
marianemcgrath <marianemcgrath@gmail.com>
2025-02-23 19:23:59 +0000

    Lab_wk02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,14 @@
+'''
+name = input("Enter your name: ")
+age = input ("Enter your age: ")
+print(f"Hello {name},\tyour age is {age}.")
+
+'''
+'''
+result = 21-4
+print (result)
+'''
+result = 2==2
+print (result)
+
+
