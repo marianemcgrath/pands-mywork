ddfd440ebf34d815dd542b4eb8c1ffeea651c34b
marianemcgrath <marianemcgrath@gmail.com>
2025-03-15 22:38:11 +0000

    Lab1.2_week3

---
+++ week03/sub.py
@@ -0,0 +1,9 @@
+# Program to subtract a number from another
+
+# input reads in a string so we can convert it into an integer
+# so we can perform mathematical operations
+
+x = int (input("Enter the first number"))
+y = int (input ("Enter the second number"))
+answer = x - y
+print ("{} minus {} is {} ".format(x,y, answer))
