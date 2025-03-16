3056ade1a8fb1c56f0f5b1ccb14e9d8c372f4504
marianemcgrath <marianemcgrath@gmail.com>
2025-03-16 18:58:29 +0000

    Lab1.3_week3_finalone

---
+++ week03/questions.py
@@ -0,0 +1,9 @@
+# Why is eggs a valid variable name while 100 is invalid?
+# Answer: 100 starts with a number, and it is not allowed for variables. Also, the number 100 is an integer number,
+# to be a variable, the number would have to be written as "onehundred".
+
+
+
+
+# What three functions can be used to get the integer, floating-point number, or string version of a value?
+# Answer: int, float, str.
